d7ac1003266261a9cebbc4c2a155fab768efc83a
minong00 <s24039@gsm.hs.kr>
2024-11-25 18:17:18 +0900

    :<..

---
+++ src/Name/name.java
@@ -0,0 +1,26 @@
+package Name;
+
+public class name {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        //int if는 사용 불가=자바 키워드 사용XXX
+        //  한글 사용가능
+        int 바보=4;
+
+        //  $사용가능
+        int $a=5;
+
+        //  _사용가능
+        int _a=6;
+
+        //대소문자 구별
+        int Sa=4;
+        int sa=44;
+
+        //  길이가 무한대임
+        int daasdasdfaswnfafnaefjkndkjawjdknawjkndjkawdnajkd=4;
+    }
+
+}
+
