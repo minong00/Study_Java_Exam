72e3e83de2beffc158bbd23336d390209214c6c2
minong00 <s24039@gsm.hs.kr>
2024-11-26 21:17:20 +0900

    입력

---
+++ src/aGibon/fScanner/scan.java
@@ -0,0 +1,11 @@
+package aGibon.fScanner;
+import java.util.Scanner;
+
+public class scan {
+    //패키지 아래에 를 써서 한다음에 메인문에
+    public static void main(String[] args) {
+        Scanner 이름 = new Scanner(System.in);
+        int a = 이름.nextInt();
+        //이런식으로 쓴다.
+    }
+}
---
+++ src/aGibon/fScanner/scanType.java
@@ -0,0 +1,14 @@
+package aGibon.fScanner;
+
+public class scanType {
+    // 이름.next() = String인데 공백을 기준으로
+    // 이름.nextLine() = String인데 한줄 전체
+    // 이름.nextByte() = byte
+    // 이름.nextShort() = short
+    // 이름.nextInt() = int
+    // 이름.nextLong() = long
+    // 이름.nextFloat() = float
+    // 이름.nextDouble() = double
+
+
+}
