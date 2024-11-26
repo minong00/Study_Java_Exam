1076dfd8471bb580d59f45c12663fdc3ab6dba31
minong00 <s24039@gsm.hs.kr>
2024-11-26 21:16:44 +0900

    반복

---
+++ src/b/bBanBok/BreakContinue.java
@@ -0,0 +1,10 @@
+package b.bBanBok;
+
+public class BreakContinue {
+    //반복을 그만하고 싶을때 쓰는것
+    //break; 다 이걸 쓰면 반복문이 멈춘다
+
+    //이 반대로는
+    //continue; 가 있는데 이건
+    // 아래를 수행하지 않고 위로 올라간다
+}
---
+++ src/b/bBanBok/banbok.java
@@ -0,0 +1,15 @@
+package b.bBanBok;
+
+public class banbok {
+    //while(조건) = 증감식이 없음
+    //횟수가 불확정할 때 사용
+
+    //do 행위
+    //while(조건)
+    //조건이 안맞아도 do는 한번 하고 종료된다.
+
+    //for(int i;조건;증감)
+    //횟수가 정해져있을 때 좋음
+    //포문을 주로 사용함
+
+}
