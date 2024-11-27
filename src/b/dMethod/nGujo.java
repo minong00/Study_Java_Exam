134bb9907f5ceba7de60f44a4ba45c0f651c3a4a
minong00 <s24039@gsm.hs.kr>
2024-11-27 21:05:16 +0900

    구조

---
+++ src/b/dMethod/nGujo.java
@@ -0,0 +1,14 @@
+package b.dMethod;
+
+public class nGujo {
+    // 1        2    3    4   5
+    //public static void 이름 () {
+    //본체
+    //}
+
+    // 1= 접근 지정자
+    // 2= 객체 없이 실행가능
+    // 3= 반환타입
+    // 4= 매서드 이름
+    // 5= 매개변수
+}
