262e9af2698be207fed0680c055b579c9738cde3
minong00 <s24039@gsm.hs.kr>
2024-11-27 21:03:36 +0900

    개선된 스위치문

--- src/b/cSwitch/fixedSwitch.java
+++ src/b/cSwitch/fixedSwitch.java
@@ -3,15 +3,24 @@ package b.cSwitch;
 public class fixedSwitch {
     public static void main(String[] args) {
         int a=1;
+
+        String b= "2";
         switch (a) {
-            case 1 -> a=3;//이런식으로 사용한다
+            case 1 -> a=3;
             case 2 -> a=4;
             case 3 -> a=5;
             case 4 -> a=6;
             case 5 -> a=7;
-
-            default -> a=8;
         }
-
+        /*
+        b=switch (a) {
+            //case 1 ->
+                //yield "2";
+            case 2 -> a=4;
+            case 3 -> a=5;
+            case 4 -> a=6;
+            case 5 -> a=7;
+        }
+*/
     }
 }
